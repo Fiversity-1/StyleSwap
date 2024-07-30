458632415d6d27c32a0f4ce1d6606ae4407fd808
86LAK <lakshan392@gmail.com>
2024-07-30 15:44:52 +1000

    buttons on bottom navbar functionality

--- lib/widgets/custom_bottom_nav_bar.dart
+++ lib/widgets/custom_bottom_nav_bar.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
 
-class CustomBottomNavBar extends StatelessWidget {
+class CustomBottomNavBar extends StatefulWidget {
   const CustomBottomNavBar({super.key});
+
+  @override
+  CustomBottomNavBarState createState() => CustomBottomNavBarState();
+}
+
+class CustomBottomNavBarState extends State<CustomBottomNavBar> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    switch (index) {
+      case 0:
+        Navigator.pushNamed(context, '/search');
+        break;
+      case 1:
+        Navigator.pushNamed(context, '/message');
+        break;
+      case 2:
+        Navigator.pushNamed(context, '/profile');
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +37,8 @@ class CustomBottomNavBar extends StatelessWidget {
             icon: Icon(Icons.messenger_rounded), label: 'Message'),
         BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
       ],
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
     );
   }
 }
