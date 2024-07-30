8b72bacbfc28a420220b796b46850c1c46fefc55
Rowan Gray <github@rowangray.au>
2024-07-30 22:32:20 +1000

    Added im_stepper to provide a visual indicator of the current step in the add clothing item page.

--- lib/features/clothing/presentation/add_clothing_item.dart
+++ lib/features/clothing/presentation/add_clothing_item.dart
@@ -1,9 +1,48 @@
 import 'package:clothing_swap/widgets/image_selection.dart';
 import 'package:flutter/material.dart';
 import 'package:clothing_swap/widgets/custom_bottom_nav_bar.dart';
+import 'package:im_stepper/stepper.dart';
 
-class AddClothingItemPage extends StatelessWidget {
+class AddClothingItemPage extends StatefulWidget {
   const AddClothingItemPage({super.key});
+
+
+  @override
+  State<StatefulWidget> createState() => _AddClothingItemPageState();
+
+
+}
+
+class _AddClothingItemPageState extends State<AddClothingItemPage> {
+  int activeStep = 0;
+
+  bool isMaxStep() {
+    return activeStep >= (getSteps().length - 1);
+  }
+
+  void nextStep() {
+    if (isMaxStep()) {
+      return;
+    }
+
+    setState(() {
+      activeStep++;
+    });
+  }
+
+  bool isFirstStep() {
+    return activeStep <= 0;
+  }
+
+  void previousStep() {
+    if (isFirstStep()) {
+      return;
+    }
+
+    setState(() {
+      activeStep--;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,9 +52,65 @@ class AddClothingItemPage extends StatelessWidget {
         title: const Text('Add Item'),
         backgroundColor: const Color.fromRGBO(255, 87, 87, 1),
       ),
-      body: const Center(
-        child: ImageSelection()
+      body: Padding(
+
+        padding: EdgeInsets.all(32),
+        child: Column(
+    children: [DotStepper(
+        dotCount: getSteps().length,
+        dotRadius: 10,
+        activeStep: activeStep,
+        shape: Shape.circle,
+        spacing: 10,
+        indicator: Indicator.shift,
+        /// TAPPING WILL NOT FUNCTION PROPERLY WITHOUT THIS PIECE OF CODE.
+        onDotTapped: (tappedDotIndex) {
+          setState(() {
+            activeStep = tappedDotIndex;
+          });
+        },
+
+        fixedDotDecoration: const FixedDotDecoration(
+          color: Colors.grey,
+        ),
+        indicatorDecoration: const IndicatorDecoration(
+            color: Colors.redAccent,
+            strokeColor: Colors.redAccent,
+            strokeWidth: 0
+        ),
+
       ),
-    );
+    /// Jump buttons.
+    Expanded(
+    child: Padding(padding: const EdgeInsets.all(18.0), child: getSteps()[activeStep])
+    ),
+
+    // Next and Previous buttons.
+    Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      ElevatedButton(onPressed: isFirstStep() ? null : previousStep, child: const Text("Previous")),
+    ElevatedButton(onPressed: isMaxStep() ? null : nextStep, child: const Text("Next")),
+    ],
+    )])));
+  }
+
+
+  static List<Widget> getSteps() {
+    return <Widget>[
+      const ImageSelection(),
+      const Center(
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Brand',
+              ),
+            ),
+          ],
+        )
+      )
+    ];
   }
 }
