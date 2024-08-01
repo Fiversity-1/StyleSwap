e75cbfa8a418b0e6b7a5bf12e717f4ee5bb2d598
Rowan Gray <github@rowangray.au>
2024-08-01 14:16:36 +1000

    Fixed naming issue

--- lib/features/clothing/presentation/add_clothing_item.dart
+++ lib/features/clothing/presentation/add_clothing_item.dart
@@ -1,4 +1,4 @@
-import 'package:clothing_swap/features/clothing/domain/ClothingInfo.dart';
+import 'package:clothing_swap/features/clothing/domain/clothing_info.dart';
 import 'package:clothing_swap/widgets/image_selection.dart';
 import 'package:flutter/material.dart';
 import 'package:clothing_swap/widgets/custom_bottom_nav_bar.dart';
