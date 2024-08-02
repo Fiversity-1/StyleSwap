6da1ad0fab66c5da8171dc884c4dd4189391c119
Rowan <github@rowangray.au>
2024-08-02 10:18:38 +1000

    Fixed unused import

--- lib/features/signup/presentation/signup.dart
+++ lib/features/signup/presentation/signup.dart
@@ -1,5 +1,4 @@
 // signup.dart
-import 'package:clothing_swap/widgets/custom_bottom_nav_bar.dart';
 import 'package:clothing_swap/widgets/custom_top_app_bar.dart';
 import 'package:flutter/material.dart';
 
