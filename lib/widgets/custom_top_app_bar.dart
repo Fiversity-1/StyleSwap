918ad713d31a348450c4fbd1f7dd4912ed4ce125
Jacob <jacob.a.poole@gmail.com>
2024-08-17 00:01:44 +1000

    Commenting two usage of provider to test if error persists

--- lib/widgets/custom_top_app_bar.dart
+++ lib/widgets/custom_top_app_bar.dart
@@ -1,7 +1,7 @@
-import 'package:clothing_swap/theme/theme.dart';
-import 'package:clothing_swap/theme/theme_switcher.dart';
+// import 'package:clothing_swap/theme/theme.dart';
+// import 'package:clothing_swap/theme/theme_switcher.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+// import 'package:provider/provider.dart';
 
 class CustomTopAppBar extends StatelessWidget {
   const CustomTopAppBar({super.key});
@@ -10,9 +10,7 @@ class CustomTopAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return AppBar(
       title: Image.asset(
-        Provider.of<ThemeSwitcher>(context).themeData == lightTheme
-            ? 'lib/images/hanger.png'
-            : 'lib/images/hanger_white.png',
+        'lib/images/hanger_white.png',
         height: 65,
         width: 75,
       ),
