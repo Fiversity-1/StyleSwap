83d177c0c802affd128854e7d134a871297104db
Jacob <jacob.a.poole@gmail.com>
2024-08-17 00:13:17 +1000

    Provider imports readded for appbar and homescreen

--- lib/features/signup/presentation/startpage.dart
+++ lib/features/signup/presentation/startpage.dart
@@ -1,9 +1,9 @@
 // startpage.dart
-// import 'package:clothing_swap/theme/theme.dart';
-// import 'package:clothing_swap/theme/theme_switcher.dart';
+import 'package:clothing_swap/theme/theme.dart';
+import 'package:clothing_swap/theme/theme_switcher.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
-// import 'package:provider/provider.dart';
+import 'package:provider/provider.dart';
 
 class StartPage extends StatelessWidget {
   const StartPage({super.key, required this.title});
@@ -92,7 +92,9 @@ class StartPage extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.only(top: (15.0)),
                 child: Image.asset(
-                  'lib/images/hanger_white.png',
+                  Provider.of<ThemeSwitcher>(context).themeData == lightTheme
+                      ? 'lib/images/hanger.png'
+                      : 'lib/images/hanger_white.png',
                   height: 75,
                   width: 75,
                 ),
