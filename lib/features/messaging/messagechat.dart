1572a991d1ef1773f4495f70def269c383535b1b
Jacob <jacob.a.poole@gmail.com>
2024-08-17 15:38:15 +1000

    Fixed buttons on dark theme to be white text

--- lib/features/messaging/messagechat.dart
+++ lib/features/messaging/messagechat.dart
@@ -12,8 +12,11 @@ class MessageChat extends StatefulWidget {
 
 class _MessageChatState extends State<MessageChat> {
   final _sendText = TextEditingController();
+  final _scroller = ScrollController();
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: const PreferredSize(
         preferredSize: Size.fromHeight(50),
@@ -51,6 +54,44 @@ class _MessageChatState extends State<MessageChat> {
                   ),
                 ],
               ),
+              //https://www.freecodecamp.org/news/build-a-chat-app-ui-with-flutter/ modified
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: height * 0.074),
+                  child: ListView.builder(
+                    itemCount: messages.length,
+                    shrinkWrap: true,
+                    controller: _scroller,
+
+                    padding: const EdgeInsets.only(top: 10, bottom: 10),
+                    //physics: const NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      return Container(
+                        padding: const EdgeInsets.only(
+                            left: 18, right: 18, top: 10, bottom: 10),
+                        child: Align(
+                          alignment: (messages[index].messageType == "receiver"
+                              ? Alignment.topLeft
+                              : Alignment.topRight),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: (messages[index].messageType == "receiver"
+                                  ? Colors.purple
+                                  : Colors.blue),
+                            ),
+                            padding: const EdgeInsets.all(16),
+                            child: Text(
+                              messages[index].messageContent,
+                              style: const TextStyle(fontSize: 15),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              )
             ],
           ),
           Center(
@@ -60,8 +101,8 @@ class _MessageChatState extends State<MessageChat> {
                 Padding(
                   padding: const EdgeInsets.only(bottom: 10),
                   child: SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.075,
-                    width: MediaQuery.of(context).size.width * 0.95,
+                    height: height * 0.075,
+                    width: width * 0.9,
                     child: TextField(
                       scrollPhysics: const NeverScrollableScrollPhysics(),
                       controller: _sendText,
@@ -74,7 +115,15 @@ class _MessageChatState extends State<MessageChat> {
                         suffix: IconButton(
                           icon: const Icon(Icons.send),
                           onPressed: () {
+                            setState(() => messages.add(ChatMessage(
+                                messageContent: "test",
+                                messageType: "sender")));
                             _sendText.clear();
+                            _scroller.animateTo(
+                              _scroller.position.maxScrollExtent,
+                              curve: Curves.easeOut,
+                              duration: const Duration(milliseconds: 500),
+                            );
                           },
                         ),
                       ),
@@ -89,3 +138,17 @@ class _MessageChatState extends State<MessageChat> {
     );
   }
 }
+
+class ChatMessage {
+  String messageContent;
+  String messageType;
+  ChatMessage({required this.messageContent, required this.messageType});
+}
+
+List<ChatMessage> messages = [
+  ChatMessage(messageContent: "Hello", messageType: "receiver"),
+  ChatMessage(messageContent: "Hello", messageType: "receiver"),
+  ChatMessage(messageContent: "Hello", messageType: "sender"),
+  ChatMessage(messageContent: "Hello", messageType: "receiver"),
+  ChatMessage(messageContent: "Hello", messageType: "sender"),
+];
