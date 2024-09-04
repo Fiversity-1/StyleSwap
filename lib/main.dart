418eea694e0e1b90265e5bed5d0b2e2aeeb92d83
Jacob <jacob.a.poole@gmail.com>
2024-09-05 02:30:28 +1000

    Added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -23,11 +23,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   //Provider code modified by GPT to include multiple instaces
   runApp(
@@ -62,7 +62,6 @@ class MyApp extends StatelessWidget {
     final themeSwitcher = Provider.of<ThemeSwitcher>(context);
 
     return MaterialApp(
-
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
@@ -78,7 +77,6 @@ class MyApp extends StatelessWidget {
           }
         },
       ),
-
       theme: themeSwitcher.themeData,
       routes: {
         '/startpage': (context) => const StartPage(title: 'StartPage'),
